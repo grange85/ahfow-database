35a8208be97d866ebf0f66ab4208441ff7277635
grange85 <andy@grange85.co.uk>
2015-05-25 11:22:07 +0100

    remove redundant shell scripts

--- _tracks/test1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for f in *; do
-    d="$(ggrep -Po 'wp_post_name: \K.*?(.*$)' $f).md"
-    if [ ! -f "$d" ]; then
-        mv "$f" "$d"
-    else
-        echo "File '$d' already exists! Skiped '$f'"
-    fi
-done
