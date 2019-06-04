bf79499e02588d2b8f2ce8e60b8a430399453bf8
grange85 <andy@grange85.co.uk>
2019-06-04 11:31:18 +0100

    external links

---
+++ js/ahfow.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$("article img").addClass("img-responsive");
+    $('a[href^="http://"],a[href^="https://"]')
+    	.not('a:has(img)')
+        .attr('target','_blank')
+        .addClass('extlink')
+    ;
+});
