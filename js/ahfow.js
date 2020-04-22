07e2baae31267ec2af51ffa7fb9afbc30d7c14fc
Andy Aldridge <andy@grange85.co.uk>
2020-04-22 17:37:33 +0100

    fix js

--- js/ahfow.js
+++ js/ahfow.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
-	$("article img").addClass("img-responsive");
-    $('.wrapper a[href^="http://"],.wrapper a[href^="https://"]')
-    	.not('a:has(img)')
+  $("article img").addClass("img-responsive");
+
+
+  $('article a[href^="http://"],article a[href^="https://"]')
+      .not('a:has(img)').not('article a[href*="fullofwishes"]').not('.resp-sharing-button__link')
         .attr('target','_blank')
-        .append(' <i class="fas fa-external-link-alt"></i>')
-    ;
+        .append(' <sup><i class="fas fa-external-link-alt initialism"></i></sup>');
+
+/*
+  $('article a[href*="fuzzywuzzy"]').each(function(){
+    var thisurl = $(this).prop('href');
+    $(this).prop('href', 'https://web.archive.org/web/2007+/' + thisurl);
+    });
+*/
 });
