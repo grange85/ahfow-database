ff576ccbf20ca730f501fdf5a6b53f50b755e4dd
Andy Aldridge <andy@grange85.co.uk>
2023-02-11 09:04:38 +0000

    more rebirth fixes

--- js/ahfow.js
+++ js/ahfow.js
@@ -1,16 +1,40 @@
 $(document).ready(function() {
-  $("article img").addClass("img-responsive");
 
 
-  $('article a[href^="http://"],article a[href^="https://"]')
-      .not('a:has(img)').not('article a[href*="fullofwishes"]').not('.resp-sharing-button__link')
-        .attr('target','_blank')
-        .append(' <sup><i class="fas fa-external-link-alt initialism"></i></sup>');
+	/* fix old posts for bootstrap 4 */
+	$("article img").each(function(index){
+		if(!$(this).hasClass('img-fluid')){
+		  $(this).addClass('img-fluid');
+		}
+	});
 
-/*
-  $('article a[href*="fuzzywuzzy"]').each(function(){
-    var thisurl = $(this).prop('href');
-    $(this).prop('href', 'https://web.archive.org/web/2007+/' + thisurl);
-    });
-*/
+	$("article iframe").each(function(index){
+	if(!$(this).parent('figure').length && !$(this).hasClass('bandcamp')){
+	  $(this).wrap('<figure class="figure embed-responsive"></figure>');
+		if(!$(this).hasClass('embed-responsive-item')){
+		  $(this).addClass('embed-responsive-item');
+		 }
+	 } else {
+	   if (!$(this).parent().hasClass('figure') && !$(this).hasClass('bandcamp')){
+	   	$(this).parent().addClass('figure embed-responsive');
+	   	$(this).addClass('embed-responsive-item');
+	   }
+	 }
+	});
+
+	$('article a[href^="http://"],article a[href^="https://"]')
+    	.not('a:has(img)')
+    	.not('article a[href*="fullofwishes\.co\.uk"]')
+    	.not('article a[href*="web\.archive\.org"]')
+    	.not('article a[data-linktype="background-image-link"]')
+    	.not('.resp-sharing-button__link')
+        .attr({'target': '_blank', 'title': 'external link - opens in a new tab or window'})
+        .append(' <i class="fas fa-external-link-alt"></i>');
+
+
+
+	$('article a[href^="http://web\.archive\.org"],article a[href^="https://web\.archive\.org"]')
+    	.not('a:has(img)')
+        .attr({'target': '_blank', 'title': 'internet archive wayback machine link - opens in a new tab or window'})
+        .append(' <i class="fas fa-arrow-alt-circle-left"></i>');
 });
