c58d751bff73a45e9e2c866e89bfed437d194b13
Andy Aldridge <andy@grange85.co.uk>
2026-04-26 23:20:05 +0100

    updates and fixes

--- _plugins/show-data-lookup.rb
+++ _plugins/show-data-lookup.rb
@@ -2,33 +2,36 @@ require 'csv'
 
 module Jekyll
   class CSVLookupTag < Liquid::Tag
-    def initialize(tag_name, input, tokens)
+    def initialize(tag_name, markup, tokens)
       super
       # Splits the input: "products.csv slug price"
-      @matches = /^(?<full_slug>(?<artist_slug>[-a-z]*)-.*$)/.match(input.strip)
-      @artist_slug = @matches['artist_slug']
-      @show_slug = @matches['full_slug']
-      @file_name = @artist_slug.to_s + '-shows.csv'
-      @target_cols = 'artistname,date,venue,city,state.country'.split(',')
+      @markup = markup.strip
     end
 
     def render(context)
       # Get the slug value from the page variables (e.g., page.slug)
       # Or pass a raw string
-      slug_to_find = @show_slug
-      
+      slug = context[@markup] || @markup
+      matches = /^(?<full_slug>(?<artist_slug>[-a-z]*)-.*$)/.match(slug)
+      return "Invalid slug format: #{slug}" if matches.nil?
+      artist_slug = matches['artist_slug']
+      show_slug = matches['full_slug'].strip
+      file_name = artist_slug.to_s + '-shows.csv'
+      target_cols = 'artistname,date,venue,city,state,country'.split(',')
+
       site = context.registers[:site]
-      file_path = File.join(site.source, '_data/gigography/', @file_name)
+      file_path = File.join(site.source, '_data/gigography/', file_name)
 
       if File.exist?(file_path)
         CSV.foreach(file_path, headers: true) do |row|
-          if row['show-slug'] == slug_to_find
-            return @target_cols.each_with_object({}) { |col, hash| hash[col] = row[col] }
+          if row['show-slug'] == show_slug
+            result = target_cols.each_with_object({}) { |col, hash| hash[col] = row[col] }
+            context.scopes.last['showdata'] = result
+            return ""
           end
         end
-        return 'blah'
+        return "No row matching slug: [#{show_slug}] found in #{file_name}"
       end
-
       "Data not found" + file_path.to_s
     end
   end
